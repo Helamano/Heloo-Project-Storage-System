c5a2ee0555f7b68547e90d90871620b00f196426
Helamano <82629903+Helamano@users.noreply.github.com>
2021-04-29 12:49:25 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+const moment = require('moment');
+const article = require('./models/article');
+
+mongoose.connect('mongodb://localhost/projects', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+});
+
+app.use((req, res, next) => {
+	res.locals.moment = moment;
+	next();
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ viabilidade: 'desc' })
+    res.render('articles/index', { articles: articles })
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(5000);
